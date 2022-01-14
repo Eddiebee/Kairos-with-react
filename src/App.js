79a7dcd34b5fb9163fded3c3848c2c971b5ba227
Eddiebee <edbeeotudor@gmail.com>
2022-01-14 10:29:49 +0100

    Integrate useGeolocation Hook from React Recipes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import { useState, useEffect } from "react";
+import { useGeolocation } from "react-recipes";
 import Header from "./components/Header";
 import WeatherInfo from "./components/WeatherInfo";
 
 const API_KEY = process.env.REACT_APP_API_KEY;
 
 function App() {
-  const city = "Port Harcourt";
-  const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
-
   const [name, setName] = useState("");
   const [temp, setTemp] = useState("");
   const [desc, setDesc] = useState("");
   const [time, setTime] = useState("");
 
+  // useGeolocation Hook
+  const { latitude, longitude } = useGeolocation(true);
+
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(API_URL);
+        const response = await fetch(
+          `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+        );
         if (!response.ok) throw Error("Did not receive expected data.");
         const data = await response.json();
         const { name, main, weather } = data;
@@ -32,18 +35,24 @@ function App() {
     };
 
     fetchData();
-  }, [API_URL]);
+  }, [latitude, longitude]);
 
-  // Update time after every sec.. wow!
-  setTimeout(() => {
-    const time = new Date().toLocaleTimeString();
-    setTime(time);
-  }, 1000);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      const time = new Date().toLocaleTimeString();
+      setTime(time);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  });
 
   return (
     <div className="App">
       <Header title="Kairos" />
       <WeatherInfo name={name} temp={temp} desc={desc} time={time} />
+      <code>
+        latitude: {latitude} longitude: {longitude}
+      </code>
     </div>
   );
 }
