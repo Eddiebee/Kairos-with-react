430d5c3468509680f00f9a63ffc96f1ac027a1f5
Eddiebee <edbeeotudor@gmail.com>
2022-01-22 16:03:32 +0100

    Add Timer component independent

--- src/components/WeatherInfo.js
+++ src/components/WeatherInfo.js
@@ -1,11 +1,12 @@
 import { BsCircle } from "react-icons/bs";
 import Aside from "./Aside";
+import Timer from "./Timer";
 
 const WeatherInfo = ({ name, temp, desc, time, icon }) => {
   return (
     <section className="weather-info container wrapper">
       <div>
-        <p id="time">{time}</p>
+        <Timer />
         <h2 className="city">{name}</h2>
         <p className="fw-bold degree">{temp}</p>
         <p className="desc">{desc}</p>
